95f4215b81a706ad1cc8ab8b5f965850e567adff
MARCOS ADRIANO FARIA COZATTI <marcos.afcozatti@senacsp.edu.br>
2024-10-09 21:07:48 -0300

    Versão com 3 exercícios iniciais da lista 1

--- ListaExerciciosLogicaProgramacao/Lista1.cs
+++ ListaExerciciosLogicaProgramacao/Lista1.cs
@@ -10,7 +10,11 @@ namespace ListaExerciciosLogicaProgramacao
     {
         internal void Exe1()
         {
+            //1) Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
             Console.WriteLine("Exercício 1");
+            Console.WriteLine("---------------");
+            Console.WriteLine("Ola mundo!");
+            Console.WriteLine("---------------");
 
             Console.WriteLine("Fim do exercício 1");
             Console.WriteLine("-------------------------------------------------------------");
@@ -18,7 +22,20 @@ namespace ListaExerciciosLogicaProgramacao
 
         internal void Exe2()
         {
+            /*2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem 
+             * de boasvindas para ela:
+                 Ex:
+                 Qual é o seu nome? João da Silva
+                 Olá João da Silva, é um prazer te conhecer!
+            */
             Console.WriteLine("Exercício 2");
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Entre com o nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá " + nome + ", é um prazer te conhecer!");
+            Console.WriteLine("---------------");
+
 
             Console.WriteLine("Fim do exercício 2");
             Console.WriteLine("-------------------------------------------------------------");
@@ -26,7 +43,24 @@ namespace ListaExerciciosLogicaProgramacao
 
         internal void Exe3()
         {
+            /*
+             * 3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no
+                final uma mensagem.
+                Ex:
+                Nome do Funcionário: Maria do Carmo
+                Salário: 1850,45
+                O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+             * 
+             */
             Console.WriteLine("Exercício 3");
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Nome do Funcionário e saário: ");
+            string nome = Console.ReadLine();
+            decimal sal = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("O funcionário(a) " + nome + " tem o salário de " + sal + " em Novembro");
+            Console.WriteLine("---------------");
+
 
             Console.WriteLine("Fim do exercício 3");
             Console.WriteLine("-------------------------------------------------------------");
